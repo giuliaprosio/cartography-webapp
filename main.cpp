816d27ccb2b89b5ba3018e6e42d7ee0fabf1f7a5
Giulia Prosio <giulia.prosio@lip6.fr>
2023-07-22 13:07:17 +0200

    Created thread listener for incoming data

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 #include <unordered_map>
 #include <string>
 #include "crow_all.h"
-
-//socket programming
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <thread>
+#include <mutex>
+#include <list>
+#include "curl4.hpp"
 
 using namespace std;
 using namespace crow::json;
@@ -18,35 +23,177 @@ struct GpsRecord {
     unsigned long lastSeen;
 };
 
-
-
-
 std::unordered_map<std::string, GpsRecord> gpsRecords;
+std::list<GpsRecord> listOfRecords;
 
-using gpsObject = std::unordered_map< std::string, wvalue >;
-using gpsList = std::vector< wvalue >;
 
 GpsRecord rec;
+
+std::mutex mtx;
+
+void broadcastFunc(struct GpsRecord rec){
+    int portNum = 5555;
+
+    int otherSocketFd = socket(AF_INET, SOCK_DGRAM, 0);
+    int broadcastEnable = 1;
+
+    if(otherSocketFd < 0){
+        std::cerr << "Couldn't create socket" << std::endl;
+        return;
+    }
+
+    int ret = setsockopt(otherSocketFd, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
+    if(ret < 0){
+        std::cerr << "Error in setting broadcast option" << endl;
+        return;
+    }
+
+    struct sockaddr_in server_addr{};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(portNum);
+    inet_aton("10.255.255.255", &server_addr.sin_addr);
+    //server_addr.sin_addr.s_addr = inet_addr("10.255.255.255");
+
+    int n;
+    socklen_t len;
+
+    std::cout << "About to send data in broadcast" << server_addr.sin_addr.s_addr << std::endl;
+    sendto(otherSocketFd, (const char *)&rec, sizeof(rec), MSG_CONFIRM, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+    std::cout << "sent" << rec.lat << std::endl;  //let's see what's printing
+    close(otherSocketFd);
+    std::cout << "closed" << std::endl;
+
+    return;
+
+}
+
+void insertGpsOther(struct GpsRecord recOfOtherUser){
+    /*GpsRecord recOfOtherUser;
+
+    const char *received = "Correctly received by server";
+
+    struct sockaddr_in cliaddr;
+    socklen_t len;
+    int n;
+
+    len = sizeof(cliaddr);
+    n = recvfrom(socketFd, (char *)&recOfOtherUser, sizeof(recOfOtherUser), MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
+
+    std::string ipOfOther = (recOfOtherUser.ipAddr);
+    std::cout << "client: " << sizeof(recOfOtherUser) << std::endl; */
+
+    //int a = 0;
+    while(true){
+        if(mtx.try_lock()){
+            gpsRecords[recOfOtherUser.ipAddr] = recOfOtherUser;
+            listOfRecords.push_back(recOfOtherUser);
+            mtx.unlock();
+            return;
+        }else{
+            //a++;
+            sleep(2);
+        }
+        return;
+    }
+
+}
+
+void listenerFunction() {
+    //creates a thread for each socket incoming
+    int socketFd;
+    int portNum = 5555;
+
+    struct sockaddr_in servaddr;
+
+    //create socket
+    if ((socketFd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        std::cerr << "Couldn't create socket" << std::endl;
+        return;
+    }
+    std::cout << "Done up to this point" << std::endl;
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(portNum);
+
+    //binding server side
+    if (bind(socketFd, (const struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        std::cerr << "Couldn't bind" << std::endl;
+        return;
+    }
+
+    std::cout << "managed to bind" << std::endl;
+
+    while (true){
+        try {
+            std::cout << "ready to receive from client" << std::endl;
+            GpsRecord recOfOtherUser;
+
+            //const char *received = "Correctly received by server";
+
+            struct sockaddr_in cliaddr;
+            socklen_t len;
+            int n;
+
+            len = sizeof(cliaddr);
+            n = recvfrom(socketFd, (char *) &recOfOtherUser, sizeof(recOfOtherUser), MSG_WAITALL,
+                         (struct sockaddr *) &cliaddr, &len);
+            if(n <= 0 ){ //no client data to process
+                std::cout << "no other user data to process";
+                sleep(2);
+                continue;
+            }else{
+                std::string ipOfOther = (recOfOtherUser.ipAddr);
+                std::cout << "client: " << sizeof(recOfOtherUser) << std::endl;
+
+                std::thread gpsOtherThread(insertGpsOther, recOfOtherUser);
+                gpsOtherThread.detach();
+            }
+
+        } catch (std::exception &err) {
+            std::cerr << "Generic error" << std::endl;
+        }
+    }
+}
 
 int main()
 {
     crow::App<crow::CORSHandler> app;
 
+    //socket managing as server side: waiting for other routers
+    //to send data of other users
+
     //define your endpoint at the root directory (app, url)
+
     CROW_ROUTE(app, "/")([](crow::response& res){
         res.set_static_file_info("static/index.html");
         res.end();
     });
 
-    CROW_ROUTE(app, "/graphNetwork")([](crow::response& res){
-                        res.set_static_file_info_unsafe("../network/netgraph.xml"); //netgraph
-                        res.end();
-                    });
 
-    /*CROW_ROUTE(app, "/gpsLastSeen")([](crow::response& res){
-        res.set_static_file_info("network/gpsPompier.json"); //netgraph
-        res.end();
-    }); */
+    CROW_ROUTE(app, "/graphNetwork")([](const crow::request& req){
+                        curl4::CURL4 init = curl4::easy::init();
+
+                        std::string url = "https://localhost:443/admin/netgraph";
+
+                        std::string xml;
+                        struct curl_slist *headers = nullptr;
+                        headers = curl_slist_append(headers, "Authorization: Basic YWRtaW46YjRtZXNocm9vdA==");
+                        init.setopt(CURLOPT_URL, url);
+                        init.setopt(CURLOPT_SSL_VERIFYPEER, 0);
+                        init.setopt(CURLOPT_SSL_VERIFYHOST, 0);
+                        init.setopt(CURLOPT_HTTPHEADER, headers);
+                        init.setopt(CURLOPT_WRITEFUNCTION, curl4::easy::writefunc);
+                        init.setopt(CURLOPT_WRITEDATA, &xml);
+
+                        CURLcode res = curl4::easy::perform(init);
+
+                        std::cout << res;
+
+                        return crow::response{xml};
+
+
+                    });
 
     CROW_ROUTE(app, "/gps")
             .methods("POST"_method)
@@ -63,21 +210,21 @@ int main()
                             rec.acc = payload["acc"].d();
                             rec.lastSeen = payload["last_seen"].u();
 
-                            gpsRecords[rec.ipAddr] = rec;
-                            //gpsObject [rec.ipAddr] = rec;
+                            while(true){
+                                if(mtx.try_lock()){
+                                    gpsRecords[rec.ipAddr] = rec;
+                                    listOfRecords.push_back(rec);
+                                    std::cout << "my GPS updated" << std::endl;
 
-                            cout << rec.lat;
-
-                            /*TODO: send and receive the struct to/from all the other routers in broadcast and ask in localhost to other server for xml
-                             * both opening a socket
-                             * 1_ localhost
-                             * 2_ consider the broadcast address
-                             *      2.1_ send GPSrecord rec in broadcast once you receive it from the user -- only if it's not the same as the one in the struct i have
-                             *           already (match ip, lat, lng)
-                             *      2.2_ add/update GPSrecord rec received from other router to GPSstruct to send to user
-                             * all this done with socket so not Crow - also very problematic with with windows so it makes sense
-                             * to work on it directly on ubuntu
-                             *      */
+                                    std::thread threadBroadcastSend(broadcastFunc, rec);
+                                    threadBroadcastSend.join();
+                                    mtx.unlock();
+                                    break;
+                                }else{
+                                    sleep(2);
+                                }
+                                break;
+                            }
 
                         } catch (std::exception &err) {
                             CROW_LOG_INFO << "Error while parsing GPS record";
@@ -109,10 +256,29 @@ int main()
     });
 
     // router listen thread creation here
+    try {
+        std::cout << "creating a listener thread";
+        std::thread threadListener(listenerFunction);
+        threadListener.detach();
+    }catch(std::exception &err){
+        cout << "Can't create the thread";
+    }
 
     //set the port, set the app to run on multiple threads, and run the app
-    app.port(18080).multithreaded().run();  //run_async() for asynchronous updates - useful with communication also
-                                                 // on socket
+    app.port(18080).multithreaded().run_async();  //run_async() for asynchronous updates - useful with communication also
+                                                        // on socket
 
+    //while(true){
+    /*try {
+        std::cout << "creating a listener thread";
+        std::thread threadListener(listenerFunction);
+        threadListener.detach();
+    }catch(std::exception &err){
+        cout << "Can't create the thread";
+    }*/
+    //}
+
+
+    //return 0;
 
 }
