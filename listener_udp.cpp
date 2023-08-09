c55c903418b964c28f1a638d30da24cc395438fa
Giulia Prosio <giulia.prosio@lip6.fr>
2023-08-09 17:09:50 +0200

    Cleared code

--- listener_udp.cpp
+++ listener_udp.cpp
@@ -37,38 +37,6 @@ void listener(){
             //const char *received = "Correctly received by server";
             char host[NI_MAXHOST];
             thisServerIP(host);
-            //----------------------------------------
-            /*struct ifaddrs *ifaddr, *ifa;
-            int family, s;
-
-
-            if (getifaddrs(&ifaddr) == -1)
-            {
-                perror("getifaddrs");
-                exit(EXIT_FAILURE);
-            }
-
-
-            for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next)
-            {
-                if (ifa->ifa_addr == NULL)
-                    continue;
-
-                s=getnameinfo(ifa->ifa_addr,sizeof(struct sockaddr_in),host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
-
-                if((strcmp(ifa->ifa_name,"wlp3s0")==0)&&(ifa->ifa_addr->sa_family==AF_INET))
-                {
-                    if (s != 0)
-                    {
-                        printf("getnameinfo() failed: %s\n", gai_strerror(s));
-                        exit(EXIT_FAILURE);
-                    }
-                    printf("\tInterface : <%s>\n",ifa->ifa_name );
-                    printf("\t  Address : <%s>\n", host);
-                }
-            } */
-            //freeifaddrs(ifaddr);
-            //---------------------------------------
 
             struct sockaddr_in cliaddr;
             socklen_t len;
