10f608ac90b1d571dc269b4337582ff130f16336
Nicolas Peugnet <nicolas@club1.fr>
2023-09-11 16:07:23 +0200

    add sourcemaps to frontend bundles

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,23 +2,26 @@ const path = require('path');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, '../out/static'),
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new NodePolyfillPlugin(),
-    new CopyPlugin({
-      patterns: [
-        "index.html",
-        "stylesheet.css",
-        "node_modules/leaflet/dist/leaflet.css",
-      ],
-      options: {
-        concurrency: 100,
-      },
-    }),
-],
+module.exports = function(_, argv) {
+  return {
+    entry: './index.js',
+    output: {
+      path: path.resolve(__dirname, '../out/static'),
+      filename: 'bundle.js',
+    },
+    devtool: argv.mode === 'production' ? 'source-map' : 'eval',
+    plugins: [
+      new NodePolyfillPlugin(),
+      new CopyPlugin({
+        patterns: [
+          "index.html",
+          "stylesheet.css",
+          "node_modules/leaflet/dist/leaflet.css",
+        ],
+        options: {
+          concurrency: 100,
+        },
+      }),
+    ],
+  };
 };
